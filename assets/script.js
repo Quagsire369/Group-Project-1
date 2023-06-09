96352b76fd520d5a1634bf512b9f654f50d860a6
Quagsire369 <131630214+Quagsire369@users.noreply.github.com>
2023-06-08 20:31:57 -0700

    added box for ingridient adder

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,11 @@ var ingredientInput = document.getElementById("ingredientInput");
 var addButton = document.getElementById("addButton");
 var shoppingList = document.getElementById("shoppingList");
 
+// Get references to the input and button elements
+var ingredientInput = document.getElementById("ingredientInput");
+var addButton = document.getElementById("addButton");
+var shoppingList = document.getElementById("shoppingList");
+
 // Add event listener to the button
 addButton.addEventListener("click", function() {
   // Get the input value
@@ -21,8 +26,27 @@ addButton.addEventListener("click", function() {
   
   // Create a new list item
   var li = document.createElement("li");
-  var textNode = document.createTextNode(ingredient);
-  li.appendChild(textNode);
+  
+  // Create a span for the ingredient text
+  var ingredientSpan = document.createElement("span");
+  ingredientSpan.textContent = ingredient;
+  
+  // Create a button for removing the item
+  var removeButton = document.createElement("button");
+  removeButton.textContent = "x";
+  removeButton.style.color = "red";
+  removeButton.addEventListener("click", function() {
+    // Remove the corresponding list item when clicked
+    shoppingList.removeChild(li);
+  });
+  
+  // Add a space between the ingredient and the remove button
+  var space = document.createTextNode(" ");
+  
+  // Append the ingredient, space, and remove button to the list item
+  li.appendChild(ingredientSpan);
+  li.appendChild(space);
+  li.appendChild(removeButton);
   
   // Add the list item to the shopping list
   shoppingList.appendChild(li);
