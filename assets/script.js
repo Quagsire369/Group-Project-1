44846e9ca23239eca1e04e3d1dcbb122c3b6fac2
Quagsire369 <131630214+Quagsire369@users.noreply.github.com>
2023-06-12 14:44:26 -0700

    worked on fuctionality of show poossible cockatils

--- assets/script.js
+++ assets/script.js
@@ -3,45 +3,45 @@ var drinkSearchButton = document.getElementById('drink-search-button');
 
 var spiritInput = $('#spirit-search');
 var spiritSearchButton = document.getElementById('spirit-search-button');
-var drinkapivar; 
+var drinkapivar;
 
 // API call for drink search
 drinkSearchButton.addEventListener("click", function (drink) {
   var drink = drinkInput.val().trim();
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response data
-        console.log(data); // You can modify this to do something with the data
-        displayDrinkRecipeUl(data);
-      })
-      .catch(error => {
-        // Handle any errors that occurred during the request
-        console.log('Error:', error);
-      });
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data
+      console.log(data); // You can modify this to do something with the data
+      displayDrinkRecipeUl(data);
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.log('Error:', error);
+    });
 });
 
 // API call for spirit search
-spiritSearchButton.addEventListener("click", function(spirit) {
+spiritSearchButton.addEventListener("click", function (spirit) {
   var spirit = spiritInput.val().trim();
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${spirit}`)
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response data
-        console.log(data); // You can modify this to do something with the data
-      })
-      .catch(error => {
-        // Handle any errors that occurred during the request
-        console.log('Error:', error);
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${spirit}`)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data
+      console.log(data); // You can modify this to do something with the data
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.log('Error:', error);
     });
 });
 
 var generateFact = document.getElementById("generate-fact");
 
 // API call for random fact
-generateFact.addEventListener("click", function() {
+generateFact.addEventListener("click", function () {
   fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
     .then(response => response.json())
     .then(data => {
@@ -50,7 +50,7 @@ generateFact.addEventListener("click", function() {
     })
     .catch(error => {
       console.log('Error:', error);
-  });
+    });
 });
 
 // Display text for random fact
@@ -67,41 +67,41 @@ function displayFact(data) {
 function displayDrinkRecipeUl(data) {
   var drinkRecipeContainer = document.querySelector(".drink-recipe-container");
   drinkRecipeContainer.innerHTML = "";
-  
-    for (var i = 0; i < data.drinks.length; i++) {
-      var drinkButtonContainer = document.createElement("ul");
-      drinkButtonContainer.setAttribute("class", "collapsible col s12");
 
-      var drinkButtonItem = document.createElement("li");
+  for (var i = 0; i < data.drinks.length; i++) {
+    var drinkButtonContainer = document.createElement("ul");
+    drinkButtonContainer.setAttribute("class", "collapsible col s12");
 
-      var drinkButtonHeader = document.createElement("div");
-      drinkButtonHeader.textContent = data.drinks[i].strDrink;
-      drinkButtonHeader.setAttribute("class", "collapsible-header")
+    var drinkButtonItem = document.createElement("li");
 
-      var drinkButtonBody = document.createElement("div");
-      drinkButtonBody.setAttribute("class", "collapsible-body");
+    var drinkButtonHeader = document.createElement("div");
+    drinkButtonHeader.textContent = data.drinks[i].strDrink;
+    drinkButtonHeader.setAttribute("class", "collapsible-header")
 
-      drinkRecipeContainer.append(drinkButtonContainer);
-      drinkButtonContainer.append(drinkButtonItem);
-      drinkButtonItem.append(drinkButtonHeader);
-      drinkButtonHeader.append(drinkButtonBody);
+    var drinkButtonBody = document.createElement("div");
+    drinkButtonBody.setAttribute("class", "collapsible-body");
 
-      drinkButtonHeader.addEventListener("click", function(e) {
-        var drink = e.target.textContent;
-        displayDrinkRecipeUl(data);
-        
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    drinkRecipeContainer.append(drinkButtonContainer);
+    drinkButtonContainer.append(drinkButtonItem);
+    drinkButtonItem.append(drinkButtonHeader);
+    drinkButtonHeader.append(drinkButtonBody);
+
+    drinkButtonHeader.addEventListener("click", function (e) {
+      var drink = e.target.textContent;
+      displayDrinkRecipeUl(data);
+
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
         .then(response => response.json())
         .then(data => {
           console.log(data);
-          document.addEventListener('DOMContentLoaded', function() {
+          document.addEventListener('DOMContentLoaded', function () {
             var elems = document.querySelectorAll('.collapsible');
             var instances = M.Collapsible.init(elems, collapsible);
           });
           displayDrinkRecipe(data);
         });
-      });
-    };
+    });
+  };
 };
 
 
@@ -131,7 +131,7 @@ function displayDrinkRecipe(data) {
 
   drinkButtonBody.append(ingredientContainer);
   ingredientContainer.appendChild(ingredient1);
-  
+
 };
 
 
@@ -141,43 +141,70 @@ function displayDrinkRecipe(data) {
 // Get references to the input and button elements
 var ingredientInput = document.getElementById("ingredient-input");
 var addButton = document.getElementById("add-button");
-var shoppingList = document.getElementById("shopping-list");
+var ingredientsOnHand = document.getElementById("shopping-list");
+// added ingredients array
+var ingredientsArray = [];
 
 // Add event listener to the button
 addButton.addEventListener("click", function() {
   // Get the input value
   var ingredient = ingredientInput.value;
-  
+
   // Create a new list item
   var li = document.createElement("li");
   
+  
+  // Create a span for the ingredient text
+
   // Create a span for the ingredient text
   var ingredientSpan = document.createElement("span");
   ingredientSpan.textContent = ingredient;
+  
+// pushing to ingredients array
+  ingredientsArray.push(ingredientInput.value);
   
   // Create a button for removing the item
   var removeButton = document.createElement("button");
   removeButton.textContent = "x";
   removeButton.style.color = "red";
-  removeButton.addEventListener("click", function() {
+  removeButton.addEventListener("click", function () {
     // Remove the corresponding list item when clicked
-    shoppingList.removeChild(li);
+    ingredientsOnHand.removeChild(li);
   });
-  
+
   // Add a space between the ingredient and the remove button
   var space = document.createTextNode(" ");
-  
+
   // Append the ingredient, space, and remove button to the list item
   li.appendChild(ingredientSpan);
   li.appendChild(space);
   li.appendChild(removeButton);
-  
+
   // Add the list item to the shopping list
-  shoppingList.appendChild(li);
-  
+  ingredientsOnHand.appendChild(li);
+ 
+
   // Clear the input field
   ingredientInput.value = "";
+
+  
 });
+
+console.log(ingredientsArray);
+
+// button to show possible cocktials
+var showButton = document.getElementById("show-button");
+
+
+// fuction for show possible drinks click
+showButton.addEventListener("click", function (){
+
+  
+
+
+
+});
+
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
@@ -210,4 +237,17 @@ fetchAllCocktails()
   .catch(error => {
     console.log('Error:', error);
   });
+
+// for (let i = 0; i < length.allCocktails; i++) {
+
+//   var test = allCocktails[i].strIngredient1;
+//   console.log(test);
+
+
+
+// }
+
+
+
+
 
